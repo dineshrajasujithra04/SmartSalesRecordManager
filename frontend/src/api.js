eb07eb7f96e8205d84ed4242e7ae0349725a0723
dineshrajasujithra04 <dineshrajasujithra04@gmail.com>
2026-09-04 21:30:57 +0530

    Update frontend API and gitignore

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://smartsalesrecordmanager.onrender.com";
+
 
 // Get all sales
 export const getSales = async () => {
